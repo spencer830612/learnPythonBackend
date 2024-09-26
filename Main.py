d5ec3b8a62cb801cc032bd8eead9cf14088b77fd
spencer830612 <asd59632+github@gmail.com>
2024-09-27 01:14:26 +0800

    0004 撰寫主程序

--- Main.py
+++ Main.py
@@ -1,4 +1,31 @@
-from Student import Student
+from LoadFile import loadStudentData
+from ScreenSelect import ScreenSelect
 
-student = Student("John", "Doe", "123")
-student.addGradeList([1, 2, 3])
+
+def informationBeforeInput(screenSelect: ScreenSelect) -> None:
+    index = 1
+    print("\n")
+    for select in screenSelect.screenSelect:
+        print(str(index) + ". " + select.description)
+        index += 1
+    print(str(index) + ". Exit")
+
+
+def getChoosenFromInput() -> int:
+    while True:
+        try:
+            choose = int(input("Please choose one and enter its number:\n"))
+            if (choose > len(screenSelect.screenSelect)):
+                print("Please enter a number in the list")
+                continue
+            break
+        except ValueError:
+            print("Please enter a number")
+    return choose
+
+
+studentList = loadStudentData()
+screenSelect = ScreenSelect(studentList)
+informationBeforeInput(screenSelect)
+choose: int = getChoosenFromInput()
+screenSelect.screenSelect[choose - 1].selectMethod()
