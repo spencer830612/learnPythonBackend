11cce7c2b7373049671987c652c64dbe0cb01f58
spencer830612 <asd59632+github@gmail.com>
2024-09-29 16:22:11 +0800

    version 0001: 內容：讀取 student01.txt，並於螢幕顯示名字、各科成績、成績總和與平均成績 花費時間：20 分鐘

---
+++ Exercise01.py
@@ -0,0 +1,24 @@
+'''
+Read the content of student01 and display the name of each student, each grade, the total grade and the average grade.
+'''
+
+FILE_PATH = './student01.txt'
+
+if __name__ == '__main__':
+    try:
+        with open(FILE_PATH, 'r') as f:
+            lines = f.read().splitlines()
+    except FileNotFoundError:
+        print(f"Error: The file {FILE_PATH} was not found.")
+        lines = []
+
+    for line in lines:
+        student_information = line.split(' ')
+        name = student_information[0]
+        first_grade = int(student_information[1])
+        second_grade = int(student_information[2])
+        third_grade = int(student_information[3])
+        sum = first_grade + second_grade + third_grade
+        average = "{:.2f}".format(sum / 3.0)
+        print(f"{name} {first_grade} {second_grade} {third_grade}, "
+              f"sum = {sum}, average = {average}")
